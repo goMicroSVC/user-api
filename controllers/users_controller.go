797a359a3869c755ee0ff6339cc9469476f6d9e0
Aly Ragab <alyragab70@gmail.com>
2021-02-14 05:19:21 +0200

    api structure

--- controllers/users_controller.go
+++ controllers/users_controller.go
@@ -1,1 +1,16 @@
 package controllers
+
+// CreateUser to create a user
+func CreateUser() {
+
+}
+
+// GetUser to Get a user
+func GetUser() {
+
+}
+
+// FindUser to find a user
+func FindUser() {
+
+}
