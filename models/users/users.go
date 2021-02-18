d20b110fb9372e001048c5e3a485d2ce2ac39fbb
Aly Ragab <alyragab70@gmail.com>
2021-02-18 03:34:23 +0200

    update

--- models/users/users.go
+++ models/users/users.go
@@ -2,7 +2,7 @@ package users
 
 // User struct is the user entity in database
 type User struct {
-	id          int64  `json:"id"`
+	ID          int64  `json:"id"`
 	FirstName   string `json:"first_name"`
 	LastName    string `json:"last_name"`
 	Email       string `json:"email"`
--- services/users_service.go
+++ services/users_service.go
@@ -1,1 +1,9 @@
 package services
+
+import (
+	"github.com/goMicroSVC/user-api/models/users"
+)
+
+func CreateUser(user users.User) (*users.User, error) {
+
+}
